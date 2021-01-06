7c206248561d48ca56fda21d4af06cd844bf01df
Wojciech <wojciechrozwalka@gmail.com>
2021-01-06 22:03:02 +0100

    model class with lombok

---
+++ src/main/java/javer/springsecurityapi/model/Quotation.java
@@ -0,0 +1,13 @@
+package javer.springsecurityapi.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Quotation {
+
+    private String content;
+    private String author;
+}
